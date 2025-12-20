7c4329577c03de42b6a2bf6beae40001b81a501e
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2025-12-20 11:58:55 +0530

    FirstProgram  Import & export
    FirstProgram  Import & export

---
+++ 19-12-2025/01practice.js
@@ -0,0 +1,5 @@
+const mathModule = require("./02math");
+
+// calls the add & sub function from mathModule
+console.log("Math value is:", mathModule.add(5, 8));
+console.log("Math value is:", mathModule.sub(5, 8));
---
+++ 19-12-2025/02math.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    return a + b;
+}
+function sub(a, b) {
+    return a - b;
+}
+
+// module.exports is used in Node.js to export functions
+module.exports = { add, sub };
