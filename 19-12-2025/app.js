1dfc7ae6ffb427d3f58cb12f1f0046738a892efc
Jyotirajsutar <141478954+Jyotirajsutar@users.noreply.github.com>
2025-12-22 00:37:15 +0530

    RemoveFile
    RemoveFile

--- 19-12-2025/app.js
+++
@@ -1,9 +0,0 @@
-// Require and import the math module from the local file './math.js'
-const mathModule = require("./Math");
-
-// Call the add and subtract functions from the mathModule
-// Log the result of adding 5 and 8 to the console using the add function from mathModule
-console.log("Math value is:", mathModule.add(5, 8));
-
-// Log the result of subtracting 8 from 5 to the console using the sub function from mathModule
-console.log("Math value is:", mathModule.sub(5, 8));
